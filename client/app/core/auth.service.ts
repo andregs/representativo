eb9960fc3203de6d756a375952b0c535fe213abc
André Gomes <andregs@users.noreply.github.com>
2017-02-21 15:00:05 -0600

    (em curso) aprimorando auth service

--- client/app/core/auth.service.ts
+++ client/app/core/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt';
 import Auth0Lock from 'auth0-lock';
 import { Deserialize, DeserializeKeysFrom } from 'cerialize';
-import 'rxjs/add/operator/delay';
 import { Observable } from 'rxjs/Observable';
 import { bindNodeCallback } from 'rxjs/Observable/bindNodeCallback';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
@@ -16,8 +15,12 @@ export class AuthService {
   private readonly userSource = new ReplaySubject<User>(1);
 
   constructor() {
-    // this emits a user if he/she is already signed in
-    this.emitAuthenticatedUser(this.createLock());
+    if (this.authenticated) {
+      this.emitAuthenticatedUser(
+        this.createLock(),
+        localStorage.getItem('accessToken')
+      );
+    }
   }
 
   displayLoginForm(redirectUrl?: string): void {
@@ -33,8 +36,7 @@ export class AuthService {
   }
 
   get authenticated(): boolean {
-    // This searches for an item in localStorage with key == 'id_token'
-    return tokenNotExpired();
+    return tokenNotExpired(); // is there 'id_token' in localStorage?
   }
 
   logout(): void {
@@ -58,43 +60,43 @@ export class AuthService {
       });
   }
 
-  private emitAuthenticatedUser(lock): void {
-    const accessToken = localStorage.getItem('accessToken');
-    if (this.authenticated && accessToken) {
-      const getUserInfoRx: (id: string) => Observable<any>
-        = bindNodeCallback(lock.getUserInfo.bind(lock));
-      getUserInfoRx(accessToken).subscribe(
-        profile => {
-          console.log('got auth0 profile', profile);
-          DeserializeKeysFrom(User.keyTransformer);
-          const user: User = Deserialize(profile, User);
-          console.log('got auth0 User', user);
-          DeserializeKeysFrom(null);
-          this.userSource.next(user);
-          this.userSource.complete(); // FIXME should not complete
-        },
-        error => {
-          localStorage.removeItem('id_token'); // logout
-          localStorage.removeItem('accessToken');
-          this.userSource.error(error); // I think this finishes the stream
-        }
-      );
-    }
+  private emitAuthenticatedUser(lock, accessToken?: string, idToken?: string): void {
+    const getUserInfoRx: (id: string) => Observable<any>
+      = bindNodeCallback(lock.getUserInfo.bind(lock));
+
+    getUserInfoRx(accessToken)
+    // .do(() => { throw new Error('boom!'); })
+    .subscribe(
+      profile => {
+        console.log('got auth0 profile', profile);
+        DeserializeKeysFrom(User.keyTransformer);
+        const user: User = Deserialize(profile, User);
+        console.log('got auth0 User', user);
+        DeserializeKeysFrom(null);
+        this.userSource.next(user);
+        lock.hide();
+        localStorage.setItem('id_token', idToken);
+        localStorage.setItem('accessToken', accessToken);
+      },
+      (error: Error) => {
+        this.logout();
+        this.onError(lock, error);
+      }
+    );
   }
 
   private onAuthenticated(lock, authResult): void {
     console.log('autenticamos!', authResult);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('accessToken', authResult.accessToken);
-    this.emitAuthenticatedUser(lock);
+    this.emitAuthenticatedUser(lock, authResult.accessToken, authResult.idToken);
   }
 
   private onError(lock, error): void {
     console.log('oops!', error);
+    lock.hide();
     lock.show({
       flashMessage: {
         type: 'error',
-        text: error.error_description
+        text: error.error_description || error.message
       }
     });
   }

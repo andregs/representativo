284baa181909fe29a568154bf7ad13f41c581904
André Gomes <andregs@users.noreply.github.com>
2017-03-07 19:51:11 -0300

    Atualizando a chave do Travis

--- script/before-install.sh
+++ script/before-install.sh
@@ -13,6 +13,6 @@ grep -v ^ArangoDB- arango.log
 echo "export const config = { auth0: { domain: '$AUTH0_DOMAIN', clientId: '$AUTH0_CLIENT_ID' } };" > app-config.ts
 
 # Prepara a chave p/ entrar no servidor
-openssl aes-256-cbc -K $encrypted_6b4694b4e4d9_key -iv $encrypted_6b4694b4e4d9_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -k "$super_secret_password" -in deploy-key.enc -out deploy-key -d
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa

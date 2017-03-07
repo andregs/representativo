533ce85efa5d89c299798cefc71766dc17f14ed6
André Gomes <andregs@users.noreply.github.com>
2017-03-07 20:58:43 -0300

    Tentando fazer o Nightmare funcionar no Travis

--- script/before-script.sh
+++ script/before-script.sh
@@ -3,7 +3,8 @@ set -eux
 
 # Iniciando o X para nossos testes no browser
 export DISPLAY=':99.0'
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+Xvfb :99 -ac -screen 0 1280x1024x16 &
+#/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 
 # Criando o BD do app
 npm run db:create

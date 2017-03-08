850858ca71f506cae364c3b3d9b6c41405b1685d
André Gomes <andregs@users.noreply.github.com>
2017-03-07 22:13:10 -0300

    Corrigindo o nome do script de deploy

--- script/before-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-
-# Iniciando o X para nossos testes no browser
-# export DISPLAY=':99.0'
-# /sbin/start-stop-daemon --start --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-
-# Criando o BD do app
-npm run db:create

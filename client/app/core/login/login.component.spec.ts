5d412ea531144272bf4757c3134f23dd2173f7b7
André Gomes <andregs@users.noreply.github.com>
2017-03-19 13:58:15 -0300

    removendo teste de login bugado (foi p/ e2e)

--- client/app/core/login/login.component.spec.ts
+++ client/app/core/login/login.component.spec.ts
@@ -3,8 +3,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 import { CoreModule } from '../core.module';
-import tryUntil from '../../../helper/try-until';
-import setInputValue from '../../../helper/set-input-value';
 import { AuthService } from '../auth.service';
 
 describe('LoginComponent', () => {
@@ -37,30 +35,4 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // FIXME isto deveria funcionar, tanto no Chrome quanto no Nightmare
-  xit('should login the user', function (done) {
-    // não dá pra acessar pelo debugElement pq o widget do auth0 é react,
-    // então uso este helper para pesquisar direto na DOM
-    tryUntil(fixture, 'button[type=submit]')
-      .map(submit => {
-        return {
-          username: fixture.nativeElement.querySelector('input[name=username]'),
-          password: fixture.nativeElement.querySelector('input[name=password]'),
-          submit,
-        };
-      })
-      .do(el => {
-        expect(service.authenticated).toBe(false, 'not logged in yet');
-        setInputValue(el.username, process.env.TEST_USERNAME);
-        setInputValue(el.password, process.env.TEST_PASSWORD);
-        el.submit.click();
-      })
-      .flatMap(() => service.user)
-      .subscribe(user => {
-        expect(user._key).toBe(process.env.TEST_USERNAME);
-        expect(user.auth0Id).toBeTruthy('got the auth0Id');
-        expect(service.authenticated).toBe(true, "user's authenticated");
-        done();
-      });
-  });
 });
--- client/helper/set-input-value.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Altera o valor do campo, emitindo o evento `input` na DOM.
- */
-export default function setInputValue(input: HTMLInputElement, value: string): void {
-  const event = new Event('input', { target: input, bubbles: true } as any);
-  input.value = value;
-  input.dispatchEvent(event);
-}

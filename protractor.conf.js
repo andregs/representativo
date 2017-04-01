59adb7da307f0b214a252cd6fc2240d1a0f5681c
André Gomes <andregs@users.noreply.github.com>
2017-04-01 16:06:25 -0300

    Adicionando ao Travis os testes e2e

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,38 +1,37 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-/*global jasmine */
-const { SpecReporter } = require('jasmine-spec-reporter');
-
 exports.config = {
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
   allScriptsTimeout: 11000,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
-  directConnect: true,
+  directConnect: process.env.CI !== true,
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   multiCapabilities: [
-    { browserName: 'chrome' },
+    {
+      browserName: 'chrome',
+      name: `Travis #${process.env.TRAVIS_JOB_NUMBER}`,
+      build: process.env.TRAVIS_BUILD_NUMBER,
+      "tunnel-identifier": process.env.TRAVIS_JOB_NUMBER,
+      tags: ['travis']
+    },
     // { browserName: 'internet explorer', requireWindowFocus: true },
     // { browserName: 'MicrosoftEdge' },
     // { browserName: 'firefox', marionette: true },
   ],
-  baseUrl: 'http://localhost:3000/',
+  baseUrl: `http://${process.env.CI ? 'representativo' : 'localhost'}:3000/`,
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+    defaultTimeoutInterval: 30000
   },
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: function() {
     require('ts-node').register({
       project: 'e2e'
     });
-    jasmine.getEnv().addReporter(new SpecReporter({
-			spec: {
-        displayStacktrace: true
-      }
-		}));
   }
 };
